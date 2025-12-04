3035ef30a64429fa23f8b005c664ccafb4f78b91
Neo Ludolph <neocan20@gmail.com>
2025-12-04 17:57:49 +0100

    feat: implement note-taking functionality with add and delete features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,31 +4,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [note, setNote] = useState("")
+  const [notes, setNotes] = useState([])
+
+  const addNote = () => {
+    const newArray = [...notes, note]
+    setNotes(newArray)
+    setNote("")
+  }
+
+  const deleteNote = (index) => {
+    notes.filter(index)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Meine Notizen</h1>
+      <input type="text" placeholder='Your Note...' value={note} onChange={(event) => setNote(event.target.value)}/>
+      <button onClick={addNote}>
+        Save
+      </button>
+      <ul>
+        {notes.map((note, index) => (
+          <li>{note} {<button onClick={() => deleteNote(index)}>Delete</button>}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
